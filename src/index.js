086ebc67783324604938d4bdaa8ce6a783734f5b
Maximilien Hjortland <maximilienhjortland@icloud.com>
2023-01-25 19:18:41 +0100

    start screen done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@
 // BACKGROUND IMAGES
 
 const backgroundImg1 = new Image(); 
-backgroundImg1.src = "./img"
+backgroundImg1.src = "/mineful/img/canvas1.jpeg"
+const backgroundImg2 = new Image();
+backgroundImg2.src = "/mineful/img/canvas2.jpg" 
+const backgroundImg3 = new Image(); 
+backgroundImg3.src = "/mineful/img/canvas3.jpg"
+const backgroundImg4 = new Image(); 
+backgroundImg4.src = "/mineful/img/canvas4.png"
+
 
 
 // HEALTH OBSTACLES
